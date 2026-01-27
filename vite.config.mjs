4889415c469647f82d9326fa2e506b69ab004ccc
SamirAli Mukhi <90303555+samirali-mukhi@users.noreply.github.com>
2026-01-27 17:42:42 +0530

    Update vite.config.mjs

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,8 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// export default defineConfig({
+//   plugins: [react()],
+//   base: '/Mukhi-Associates/',
+// })
+
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Mukhi-Associates/',
-})
-
+  base: "/",   // IMPORTANT FOR CUSTOM DOMAIN
+});
